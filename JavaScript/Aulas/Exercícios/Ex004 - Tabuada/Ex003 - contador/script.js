773dfd1fc11428e227d46b8f07f9c9aaaa6922eb
Alisson Batista <alisson252525@hotmail.com>
2023-08-17 12:31:14 -0300

    Exercício da tabuada finalizado

--- JavaScript/Aulas/Exercícios/Ex004 - Tabuada/Ex003 - contador/script.js
+++ JavaScript/Aulas/Exercícios/Ex004 - Tabuada/Ex003 - contador/script.js
@@ -1,47 +1,19 @@
-function contar() { 
-    var inicio = document.getElementById('inicio')
-
-    var fim = document.getElementById('fim')
-
-    var passo = document.getElementById('passo')
-
-    var res = document.getElementById('resultado')
-    var i = Number(inicio.value)
-
-
-    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length==0) {
-        res.innerHTML = 'Impossível contar!'
-        window.alert('[Erro] Faltam dados')
-        
-    } else {
-        res.innerHTML = `Contando...<br>`
-        var i = Number(inicio.value)
-        var f = Number(fim.value)
-        var p = Number(passo.value)
-        if (p <= 0) { 
-            window.alert('Passo invalido. Considerando passo 1')
-            p = 1
+function tabuada() { 
+    var numero = document.getElementById('num')
+    var tab = document.getElementById('selectab')
+    if (numero.value.length == 0) {
+        window.alert('Numero inválido.')
+    } else { 
+        var num = Number(numero.value)
+        tab.innerHTML = ''
+        tab.size = 10
+        for (var i = 1; i <= 10; i++) { 
+            let item = document.createElement('option')
+            item.text = `${num} x ${i} = ${num * i}`
+            item.value = `tab${i}` //bom para saber o item selecionado
+            tab.appendChild(item)
         }
-        if (i < f) {
-            //contagem crescente
-            while (i <= f) {
-                res.innerHTML += `${i} 👉 `
-                i += p
-            }
-            res.innerHTML += `🏁`
-        } else { 
-            //contagem decrescente
-            while (i >= f) {
-                res.innerHTML += `${i} 👉 `
-                i -= p
-            }
-            res.innerHTML += `🏁`
-        }
-        
     }
+    
+    
 }
-    
-
-
-    
-
